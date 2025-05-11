90e68af61c67282e6411ed046f8baadafc97db6f
masatake <mastake.home@outlook.jp>
2025-05-11 05:12:00 +0000

    fix: 不要なコメントを削除し、ドキュメンテーションを整理

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,5 @@
-"""
-DeepReadアプリケーションパッケージ
-"""
+"""アプリケーションのメインパッケージです。"""
 
-# Celeryアプリをインポートして、Django起動時に自動的にCeleryアプリがロードされるようにする
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
